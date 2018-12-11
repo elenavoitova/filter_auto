c2a1fd6b83ca069a2fadf7d179c1b3a6649ab299
elenavoitova <elena.voitova.lb@gmail.com>
2018-12-11 12:22:56 +0200

    Update homepage.js

--- pages/homepage.js
+++ pages/homepage.js
@@ -5,62 +5,62 @@ import { candidate, filter, hiringSection } from '../utils/locators';
 export default class HomePage extends AbstractPage {
 
 	constructor(){
-		super();
-		this.clear = filter.clearButton;
-		this.submit = filter.submitButton;
-		this.searchName = filter.nameInput;
-		this.searchCity = filter.cityInput;
+	   super();
+	   this.clear = filter.clearButton;
+           this.submit = filter.submitButton;
+	   this.searchName = filter.nameInput;
+	   this.searchCity = filter.cityInput;
 
-		this.stepUp = candidate.upButton;
-		this.userName = candidate.profileName;
-		this.userPhoto = candidate.profileImage;
+	   this.stepUp = candidate.upButton;
+	   this.userName = candidate.fullName;
+	   this.userPhoto = candidate.profileImage;
+	   this.userCity = candidate.city;
+	   this.userDetails = candidate.profileContainer:
 
-		this.stage1 = hiringSection.step1;
-		this.stage2 = hiringSection.step2;
-		this.stage3 = hiringSection.step3;
-  }
+	   this.stage1 = hiringSection.step1;
+	   this.stage2 = hiringSection.step2;
+	   this.stage3 = hiringSection.step3;
+       }
 
 
 	async addFilter(name, city){
-		let result = {};
-		await this.set(this.searchName, name);
-		await this.set(this.searchCity, city);
-		await this.find(this.submit).click();
+	   let result = {};
+	   await this.set(this.searchName, name);
+	   await this.set(this.searchCity, city);
+	   await this.find(this.submit).click();
 
-
-		let items = await this.findAll('.CrewMember-container');
-		if (items.length == 0){
-				return items.length;
-		} else {
-				let person = await this.detectPerson();
-				return result = {
-									count: items.length,
-									person: person
+           let items = await this.findAll(this.userDetails);
+	   if (items.length == 0){
+		return items.length;
+	   } else {
+		let person = await this.detectPerson();
+		return result = {
+			count: items.length,
+			person: person
 			}
-		}
+	   }
 	}
 
 	async clearInputs(){
-		await this.find('#name').clear();
-		await this.find('#city').clear();
+	   await this.find(this.searchName).clear();
+	   await this.find(this.searchCity).clear();
 	}
 
 	async detectPerson(){
-		let name = await this.find('.CrewMemeber-name > div:nth-child(1)').getText();
-		let city = await this.find('.CrewMemeber-name > div:nth-child(2)').getText();
-		return `${name} ${city}`;
+	   let name = await this.find(this.userName).getText();
+	   let city = await this.find(this.userCity).getText();
+	   return `${name} ${city}`;
 	}
 
 	async removeFilter(){
-		await this.find('[type=button]').click();
-		let result = await this.find('.CrewMember-container');
-		if (result.length == 0 || result.length == 1) {
-				return false;
-		} else  if(this.find('#name').getText() !== "" && this.find('#city').getText() !== ""){
-		 		return false;
-		}
-
-		return true;
+	   await this.find(this.clear).click();
+	   let result = await this.find(this.userDetails);
+	   if (result.length == 0 || result.length == 1) {
+		return false;
+	   } else  if(this.find(this.searchName).getText() !== "" && this.find(this.searchCity).getText() !== ""){
+		 return false;
+	   }
+	  return true;
 	}
 
 
